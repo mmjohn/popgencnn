f5b73f97d6c3737840fd2d0cc5510de3183ca97e
mmjohn <mmjohn@utexas.edu>
2020-06-15 13:37:33 -0500

    Change output filename

--- R/doSim.R
+++ R/doSim.R
@@ -11,5 +11,5 @@ doSim <- function(index){
   mbp   <- 10^(runif(n = 1,min = -8, max = -6))
   my.ne <- sample(c(1000, 2000, 5000, 10000, 15000, 20000, 50000), 1)
   msSimulate(morgans.per.bp = mbp, ne = my.ne,
-             filename = 'all.LD.sims.txt' )
+             filename = 'all_LD_sims.txt' )
 }
