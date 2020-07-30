5c0f8e83a08edef071c69e64b57974cb63334180
mmjohn <mmjohn@utexas.edu>
2020-07-29 19:25:41 -0500

    Fixed issue when ms returns an alignment with 1 segregating site

--- R/get_alignment_data.R
+++ R/get_alignment_data.R
@@ -10,8 +10,15 @@ get_alignment_data <- function(all_data){
 
   # get relevant line numbers
   x1_linenum <- which(grepl("^(0|1)+(0|1)$", all_data))
-  # get relecant data
-  alignment_lines <- all_data[x1_linenum]
+  x1_linenum1 <- which(grepl("^(0|1)$", all_data))
+  x1_linenum_all <- sort(c(x1_linenum, x1_linenum1),
+                         decreasing = F)
+  #lines_seg <- which(grepl("^segsites: ", all_data))
+  #get_alignment_linenums <- function(num){seq(num+2, num+52)}
+  #linenums_align <- unlist(lapply(lines_seg,
+  #                                get_alignment_linenums))
+  # get relevant data
+  alignment_lines <- all_data[x1_linenum_all]
   # split into each simulation
   # num_sims (200 - defined above), chr_per_alignment (50 - constant)
   # split data with alignment line numbers
