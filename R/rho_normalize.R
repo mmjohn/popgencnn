4b9d1171fd8fdc8edde275fe85097fcd32f3598a
mmjohn <mmjohn@utexas.edu>
2021-02-17 17:18:37 -0600

    Added documentation for mean_center param

--- R/rho_normalize.R
+++ R/rho_normalize.R
@@ -2,6 +2,7 @@
 #'
 #' This function takes in a vector of rho values and normalizes it by log transforming and centering on a mean value
 #' @param rho_set A vector of rho values used for training, validation, or testing
+#' @param mean_center The mean used for normalization; currently using the mean of the training set
 #' @examples
 #' rho_normalize(c(7.23, 55.31, 715.52, 2328.45, 423.55), 4.2)
 #' @export
