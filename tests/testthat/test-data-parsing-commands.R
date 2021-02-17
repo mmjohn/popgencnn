997ab99ce609c5e96e6136b84540f7c32a7a7f16
mmjohn <mmjohn@utexas.edu>
2021-02-17 17:07:13 -0600

    Add simple tests for splitting and rho normalization functions

--- tests/testthat/test-data-parsing-commands.R
+++ tests/testthat/test-data-parsing-commands.R
@@ -1,6 +1,6 @@
 context("ms simulation data parsing commands")
 
-test_that("alignment commands", {
+test_that("extract alignment commands", {
 
   v1 <- parse_single_alignment(c("1010", "0010"))
   v2 <- matrix(c(1,0,0,0,1,1,0,0), ncol = 4, nrow = 2)
@@ -15,7 +15,7 @@ test_that("alignment commands", {
 
 })
 
-test_that("position commands", {
+test_that("extract position commands", {
 
   list1 <- get_pos_data(c("positions: 0.1 0.2 0.3",
                           "random -r 3.4 20001",
@@ -25,11 +25,18 @@ test_that("position commands", {
 
 })
 
-test_that("rho commands", {
+test_that("extract rho commands", {
 
   vec1 <- get_rho_data(c("cat -t 0.2 -r 1.3 20001",
                          "random -t 0.7 -r 3.4 20001"))
   vec2 <- matrix(c(0.2, 0.7, 1.3, 3.4), ncol = 2)
   expect_equal(vec1, vec2)
+
+})
+
+test_that("splitting train/test data commands", {
+
+  expect_equal(get_split_index(0.8, 1999), c(1, 1600, 1601, 1800, 1801, 1999))
+  expect_equal(get_split_index(0.92, 156275), c(1, 143773, 143774, 150024, 150025, 156275))
 
 })
--- tests/testthat/test-data-transformation-commands.R
+++ tests/testthat/test-data-transformation-commands.R
@@ -1,13 +1,16 @@
 context("alignment data transformations")
 
 test_that("sorting commands", {
+
   m_raw <- matrix(c(0,0,0,1,1,1,0,1,0), ncol = 3, nrow = 3)
   m_sort <- matrix(c(0,0,0,1,1,1,1,0,0), ncol = 3, nrow = 3)
 
   expect_equal(sort_align(m_raw), m_sort, check.attributes = F)
+
 })
 
 test_that("3D array command", {
+
   x1 <- matrix(c(1,0,0,1,1,0,0,0), nrow = 2, ncol = 4)
   x2 <- matrix(c(1,1,1,0,1,0,1,1), nrow = 2, ncol = 4)
   x3 <- matrix(c(0,1,1,0,1,0,0,1), nrow = 2, ncol = 4)
@@ -15,5 +18,19 @@ test_that("3D array command", {
 
   output <- list_to_array(x_list, c(2, 4, 3))
   expect_equal(dim(output), c(2, 4, 3))
+
+})
+
+test_that("rho normalization commands", {
+
+  rho_raw <- c(7.23, 55.31, 715.52, 2328.45, 423.55)
+  rho_norm <- c(-2.2217610, -0.1870463, 2.3730096, 3.5529581, 1.8486716)
+
+  expect_equal(rho_normalize(rho_raw, 4.2), rho_norm)
+
+  rho_raw2 <- c(715.52, 55.31, 2328.45, 7.23, 423.55)
+  rho_norm2 <- c(2.47300955, -0.08704628, 3.65295809, -2.12176096, 1.94867157)
+
+  expect_equal(rho_normalize(rho_raw2, 4.1), rho_norm2)
 
 })
